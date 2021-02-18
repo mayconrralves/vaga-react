f04be7bd6fda14d825322513d0f8d27394105e7e
Maycon Rogers Ribeiro Alves <mayconrralves@yahoo.com.br>
2021-02-18 09:21:12 -0300

    Primeiro commit, limpeza do projeto e adição do estilo global

---
+++ web-ecommerce/src/styles.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'styled-components';
+
+const globalStyle = createGlobalStyle`
+	*{
+		margin: 0;
+		box-sizing: border-box;
+	}
+	html, body {
+		width: 100%;
+		min-height: 900px;
+		background-image: linear-gradient(180deg, #22202c 0%, #402845 100%);
+		margin: 0;
+		a, h1, p, input {
+			font-family: Helvetica;
+			color: #fff;
+		}
+	}
+
+`;
