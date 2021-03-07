2c65d654611fa0f856581dfab448ba4cc3e05606
Maycon Rogers Ribeiro Alves <mayconrralves@yahoo.com.br>
2021-03-07 12:00:51 -0300

    Adição das páginas. Separando fncionalidades

---
+++ web-ecommerce/src/pages/Shop.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../components/Header';
+import Shop from '../components/Shop';
+
+
+export default function ShopPage(){
+	return (
+		<>
+			<Header/>
+			<Shop />
+		</>
+		)
+}
