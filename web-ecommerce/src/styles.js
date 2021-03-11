4b4655e94479d046dfaaea8090c4a9e138b6599a
Maycon Rogers Ribeiro Alves <mayconrralves@yahoo.com.br>
2021-03-11 07:42:20 -0300

    Troca de cores

--- web-ecommerce/src/styles.js
+++ web-ecommerce/src/styles.js
@@ -8,11 +8,11 @@ const globalStyle = createGlobalStyle`
 	html, body {
 		width: 100%;
 		min-height: 100%;
-		background-image: linear-gradient(180deg, #22202c 0%, #402845 100%);
+		background-image: linear-gradient(180deg,  #B6CB9E 0%, #D1F0B1 100%);
 		margin: 0;
 		a, h1, p, input {
 			font-family: Helvetica;
-			color: #fff;
+			color: #000;
 		}
 	}
 
