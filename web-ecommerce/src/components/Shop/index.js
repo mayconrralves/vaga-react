0b0f9548c2c3ac22ddfb7e26e260ffd3a810d16a
Maycon Rogers Ribeiro Alves <mayconrralves@yahoo.com.br>
2021-03-22 13:22:04 -0300

    Continuação da construção de shop

--- web-ecommerce/src/components/Shop/index.js
+++ web-ecommerce/src/components/Shop/index.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { Container } from './styles';
-export default function Store(){
+import { useDispatch, useSelector } from 'react-redux';
+import { getProducts } from '../../store/modules/products/actions';
+export default function Store(){	const [quantityItem, setQuantityItem] = useState(1);
+	const dispatch = useDispatch();
+	const { msgError, products} = useSelector(state=>state.products);
+	const listProducts = ( ) => {
+		return products.map(product=>(
 
+			<li key={product.id} >
+				<Link to={'/details/'+ product.id}>
+					<img src={product.img} alt={"imagem de"+ product.description}/>
+					<div className="details">
+						<h3>{product.name}</h3>
+						<p> R$ {product.price}</p>
+					</div>
+				</Link>
+			</li>
+			));
+	}
+
+	useEffect(()=>{
+		dispatch(getProducts());
+	}, []);
 	return (
 		<Container>
+			{
+				msgError ? (
+					<div className='errorMessage'>{msgError}</div>
+					):(
+					<ul>
+						{listProducts()}
+					</ul>
+					)
+			} 
 					
 		</Container>
 	)
--- web-ecommerce/src/components/Shop/styles.js
+++ web-ecommerce/src/components/Shop/styles.js
@@ -2,5 +2,49 @@ import styled from 'styled-components';
 
 
 export const Container = styled.main`
-	
+	width: 90%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	ul {
+		width: 100%;
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: space-between;
+		li {
+			width: 25%;
+			display: flex;
+			flex-direction: column;
+			margin: 10px 35px 25px 35px;
+			
+			border-radius: 6px;
+			justify-content: space-between;
+			align-items: center;
+			a {
+				text-decoration: none;
+				img {
+					width: 280px;
+					margin-top: 4px;
+					margin-bottom: 10px;
+					border-radius: 8px;
+				}
+				.details {
+					display: flex;
+					flex-direction: column;
+					align-items: center;
+					margin-bottom: 12px;
+					h3 {
+						margin: 8px 0;
+						font-size: 1.3em;
+					}
+					p {
+						margin-bottom: 2px;
+						font-size: 1em;
+						font-weight: bold;
+					}
+
+				}	
+			}
+		}
+	}	
 `;
