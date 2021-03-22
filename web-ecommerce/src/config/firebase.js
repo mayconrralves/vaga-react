f2efeb92854918caa399c25898f06343aef00b23
Maycon Rogers Ribeiro Alves <mayconrralves@yahoo.com.br>
2021-03-22 14:03:03 -0300

    Adição do firebase para uso do storage

---
+++ web-ecommerce/src/config/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCYks1I9N7xTxslbkF3-JiP_nypFhO3D7w",
+    authDomain: "e-commerce-44c28.firebaseapp.com",
+    databaseURL: "https://e-commerce-44c28-default-rtdb.firebaseio.com",
+    projectId: "e-commerce-44c28",
+    storageBucket: "e-commerce-44c28.appspot.com",
+    messagingSenderId: "895374857002",
+    appId: "1:895374857002:web:7699dc31519318a93dc453"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
