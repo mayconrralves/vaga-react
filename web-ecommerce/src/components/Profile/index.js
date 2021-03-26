2ae7d09dee29fe00dae969a059ded78b663891d4
Maycon Rogers Ribeiro Alves <mayconrralves@yahoo.com.br>
2021-03-26 14:42:50 -0300

    Adição do cadastro e atualização da foto do usuário

--- web-ecommerce/src/components/Profile/index.js
+++ web-ecommerce/src/components/Profile/index.js
@@ -1,21 +1,61 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { useDispatch, useSelector } from 'react-redux';
-import { updateUser, getUser } from '../../store/modules/user/actions';
+import { FaCamera } from 'react-icons/fa';
+
+import { updateUser, getUser, setAvatar } from '../../store/modules/user/actions';
 import { Container } from './styles';
 
 
 export default function Profile(){
 	const { user, success } = useSelector(state=> state.user);
+	const [image, setImage] = useState(null);
+	const [inputFile, setInputFile] = useState('');
 	const dispatch = useDispatch();
 	useEffect(()=> {
 		 dispatch(getUser());
 	},[]);
+	const onFileChange = e => {
+		const file = e.target.files[0];
+		setImage(file);
+	}
+	const saveFile = e => {
+		dispatch(setAvatar(image));
+	}
+	const imageSubmit = event => {
+		const file = event.target.files[0];
+		const reader = new FileReader();
+		if(file){
+			reader.readAsDataURL(file);
+			reader.onloadend = e => {
+				
+				setImage(reader.result);
+			}
+		}
+	}
+	const uploadClick = event=> {
+		inputFile.click();
+		event.preventDefault();
+	}
 	return success ? (
-			<Container>
+			<Container
+				image={  image ? true : false}
+			>
 				<h2>
 					Atualizar cadastro
 				</h2>
+				<label htmlFor='file-selector'>
+					<span><FaCamera/></span>
+					{image && <img src={image} onClick={uploadClick} />}
+				</label>
+				<input 
+					name="image"
+					type='file' 
+					id='file-selector' 
+					onChange={imageSubmit} 
+					ref={ input => setInputFile(input)}
+				/>
+
 				<Formik
 					initialValues={{
 							displayName: user.displayName,
--- web-ecommerce/src/components/Profile/styles.js
+++ web-ecommerce/src/components/Profile/styles.js
@@ -10,6 +10,37 @@ export const Container = styled.main`
 		font-size: 2.5em;
 		color: #fff;
 	}
+	label {
+		width: 20%;
+		border: 3px solid #fff;
+		border-radius: 10px;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		color: #fff;
+		font-size: 4em;
+		height: 450px;
+		span {
+				
+			display: ${props=>props.image ? 'none' : 'inline'};
+			font-weight: bold;
+			color: #FFF;
+			opacity: 0.3;
+			font-size: 2em;
+			
+			}
+		img {
+			display: ${props=>props.image ? 'inline' : 'none'};
+			border-radius: 10px;
+			width: 100%;
+			margin: 20px 0;
+		}
+	}
+	input {
+		&[type='file'] {
+	  			display: none;
+			}
+	}
 	form {
 		width: 70%;
 		height: 500px;
@@ -24,6 +55,7 @@ export const Container = styled.main`
 			margin-top: 12px;
 			font-size: 1.4em;
 			padding-left: 12px;
+			
 			&:last-child {
 			 	background-color: green;
 			 	margin-top: 20px;
