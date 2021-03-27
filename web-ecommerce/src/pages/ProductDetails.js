a631c938e0336e87836d8709307a09f8bf5765cc
Maycon Rogers Ribeiro Alves <mayconrralves@yahoo.com.br>
2021-03-27 12:35:50 -0300

    Adição da página para o carrinho

---
+++ web-ecommerce/src/pages/ProductDetails.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/Header';
+import ProductDetails from '../components/ProductDetails';
+
+
+
+export default function ProductDetailsPage(){
+	return (
+		<>
+			<Header/>
+			<ProductDetails />
+		</>
+		)
+}
--- web-ecommerce/src/routes/index.js
+++ web-ecommerce/src/routes/index.js
@@ -8,7 +8,8 @@ import SignUp from '../components/SignUp';
 import Shop from '../pages/Shop';
 import Profile from '../pages/Profile';
 import NotFound from '../components/NotFound';
-import Details from '../components/ProductDetails';
+import Details from '../pages/ProductDetails';
+import Cart from '../pages/Cart';
 
 export default function Navigation(props){
 
@@ -19,6 +20,7 @@ export default function Navigation(props){
 				<CustomRoute path='/login' exact component={SignIn} /> 
 				<CustomRoute path='/signup' exact component={SignUp} />
 				<CustomRoute path='/details/:id' exact component={Details} />
+				<CustomRoute path='/cart' exact component={Cart}  />
 				<CustomRoute path='/profile' exact component={Profile}  protect />
 				<CustomRoute component={NotFound} />
 		</Switch>
