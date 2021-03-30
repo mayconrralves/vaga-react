32751307f69e5168a0970ef8aa7b977d3e0d2d7d
Maycon Rogers Ribeiro Alves <mayconrralves@yahoo.com.br>
2021-03-30 11:11:18 -0300

    Adição de limitação para a quantidade de itens comprados até o estoque apresentado

--- web-ecommerce/src/components/Cart/index.js
+++ web-ecommerce/src/components/Cart/index.js
@@ -2,19 +2,26 @@ import React from 'react';
 import { Container } from './styles';
 import { useDispatch, useSelector } from 'react-redux';
 import { FiMinusSquare, FiPlusSquare } from "react-icons/fi";
+import { MdDelete } from "react-icons/md";
 
 import { updateCartSum, updateCartSub, deleteProductInCart} from '../../store/modules/cart/actions';
 export default function Cart(){
 	const { products } = useSelector(state=>state.cart);
 	const dispatch = useDispatch();
 
+	const deleteProduct = index => {
+		dispatch(deleteProductInCart(index));
+	}
+
 	const updateSum = index => {
-		dispatch(updateCartSum(index));
+		if(products[index].quantityPurchase+1 <= products[index].quantity){
+			dispatch(updateCartSum(index));
+		}
 	}
 
 	const updateSub = index => {
 		if(products[index].quantityPurchase-1 < 1){
-			dispatch(deleteProductInCart(index));
+			deleteProduct(index);
 		} else {
 			dispatch(updateCartSub(index));
 		}
@@ -24,11 +31,12 @@ export default function Cart(){
 		return products.map((product,index)=>{
 			return (
 					<li key={index}>
-						<h3>{product.name}</h3>
+						<h3>{product.name} <button onClick={()=>deleteProduct(index)}><MdDelete /></button></h3>
+
 						<p><strong>Marca:</strong> {product.brand}</p>
 						<p><strong>Descrição:</strong>  {product.description}</p>
 						<p><strong>Preço:</strong>  {product.price}</p>
-						<p><strong>Quantidade: </strong> {product.quantity} </p>
+						<p><strong>Estoque: </strong> {product.quantity} </p>
 						<p>
 							<strong>Quantidade: </strong> 
 							<button onClick={()=> updateSub(index)}>
