fd9ec0fc22f73cc2aa9a9e3ab635d1ed13e1734d
Maycon Rogers Ribeiro Alves <mayconrralves@yahoo.com.br>
2021-03-31 19:05:00 -0300

    Adição dos estilos em Cart

--- web-ecommerce/src/components/Cart/index.js
+++ web-ecommerce/src/components/Cart/index.js
@@ -4,7 +4,12 @@ import { useDispatch, useSelector } from 'react-redux';
 import { FiMinusSquare, FiPlusSquare } from "react-icons/fi";
 import { MdDelete } from "react-icons/md";
 
-import { updateCartSum, updateCartSub, deleteProductInCart} from '../../store/modules/cart/actions';
+import { 
+	updateCartSum, 
+	updateCartSub, 
+	deleteProductInCart ,
+} from '../../store/modules/cart/actions';
+
 export default function Cart(){
 	const { products } = useSelector(state=>state.cart);
 	const dispatch = useDispatch();
@@ -31,23 +36,33 @@ export default function Cart(){
 		return products.map((product,index)=>{
 			return (
 					<li key={index}>
-						<h3>{product.name} <button onClick={()=>deleteProduct(index)}><MdDelete /></button></h3>
-
-						<p><strong>Marca:</strong> {product.brand}</p>
-						<p><strong>Descrição:</strong>  {product.description}</p>
-						<p><strong>Preço:</strong>  {product.price}</p>
-						<p><strong>Estoque: </strong> {product.quantity} </p>
-						<p>
-							<strong>Quantidade: </strong> 
-							<button onClick={()=> updateSub(index)}>
-								<FiMinusSquare />
-							</button>
-							{product.quantityPurchase} 
-							<button onClick={()=> updateSum(index)}>
-								<FiPlusSquare />
-							</button>
-						</p>
-						<p><strong>Total: R$ {parseFloat(product.price) * product.quantityPurchase}</strong></p>
+						<section className='title'>
+							<h3><span>{product.name}</span><span onClick={()=>deleteProduct(index)}><MdDelete /></span></h3>
+						</section>
+						<section>
+							<img src={product.img}/>
+							<div>
+								<p><strong>Marca:</strong> {product.brand}</p>
+								<p><strong>Descrição:</strong>  {product.description}</p>
+								<p><strong>Preço:</strong>  {product.price}</p>
+								<p><strong>Estoque: </strong> {product.quantity} </p>
+								<p>
+									<strong>Quantidade: </strong> 
+									<span onClick={()=> updateSub(index)}>
+										<FiMinusSquare />
+									</span>
+									{product.quantityPurchase} 
+									<span onClick={()=> updateSum(index)}>
+										<FiPlusSquare />
+									</span>
+								</p>
+								<p>
+									<strong>
+										Total: R$ {parseFloat(product.price) * product.quantityPurchase}
+									</strong>
+								</p>
+							</div>
+						</section>
 					</li>
 				)
 		});
--- web-ecommerce/src/components/Cart/styles.js
+++ web-ecommerce/src/components/Cart/styles.js
@@ -1,5 +1,57 @@
 import styled from 'styled-components';
 
 export const Container = styled.main`
-	
+	width: 100%;
+	ul {
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		margin-top: 12px;
+		li {
+			width: 70%;
+			background-color: rgba(129, 182, 34, .7);
+			margin: 6px 0;
+			padding: 6px;
+			border-radius: 6px;
+			list-style: none;
+			section {
+					width: 100%;
+					display: flex;
+					justify-content: space-around;
+					&.title {
+						justify-content: space-between;
+						margin-bottom: 12px;
+						h3 {
+							width: 100%;
+							display: flex;
+							justify-content: space-between;
+							color: #fff;
+							font-size: 1.5em;
+						}
+
+					}
+					img {
+						width: 30%;
+						border-radius: 10px;
+					}
+					div {
+						width: 50%;
+						p {
+							margin-bottom: 5px;
+							vertical-align: middle;
+							font-size: 1em;
+							color: #fff;
+							span {
+								text-align: center;
+								vertical-align: middle;
+								margin: 0 2px;
+								width: auto;
+							}
+						}
+					}
+				}
+		}
+
+	}
 `;
