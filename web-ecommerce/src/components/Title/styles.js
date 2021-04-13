f490640ac62bb23bfd988716effa8ea3687133af
Maycon Rogers Ribeiro Alves <mayconrralves@yahoo.com.br>
2021-04-13 18:45:57 -0300

    Refatoração das medias queries

--- web-ecommerce/src/components/Title/styles.js
+++ web-ecommerce/src/components/Title/styles.js
@@ -2,14 +2,18 @@ import styled from 'styled-components';
 
 export const Container = styled.h1`
 		a {
-			@media (max-width: 600px){
+			@media (max-width: 800px){
 				font-size: 1.4em;
 			}
-			@media (max-width: 600px){
+			@media (max-width: 640px){
 				font-size: 1.2em;
 			}
-			@media (max-width: 400px){
+			
+			@media (max-width: 510px){
 				font-size: 1em;
+			}
+			@media (max-width: 350px){
+				font-size: 0.9em;
 			}
 			color: #3D550C;
 			font-size: 1.6em;
