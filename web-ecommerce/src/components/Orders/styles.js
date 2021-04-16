28eef775c9f0d3908b35a813f8240192503ea8e6
Maycon Rogers Ribeiro Alves <mayconrralves@yahoo.com.br>
2021-04-16 20:25:00 -0300

    Adição de media queries para responsividade em Orders

--- web-ecommerce/src/components/Orders/styles.js
+++ web-ecommerce/src/components/Orders/styles.js
@@ -1,11 +1,27 @@
 import styled from 'styled-components';
 
+const styledMobile500 = styles => (
+    `
+        @media screen and (max-width: 500px){
+            
+            ${styles}
+        }
+    `
+)
+const styledMobile660 = () => (
+    `
+        @media screen and (max-width: 770px){
+            width: 95%;
+        }
+    `
+)
 export const Container = styled.main`
     width: 100%;
     display: flex;
     flex-direction: column;
     align-items: center;
     h2 {
+        ${styledMobile660()}
         width: 70%;
         height: 50px;
         color: #fff;
@@ -23,9 +39,10 @@ export const Container = styled.main`
         align-items: center;
         padding: 0;
         li {
+            ${styledMobile660()}
+            width: 70%;
             background-color: rgba(129, 182, 34, .7);
             height: auto;
-            width: 70%;
             margin: 10px 0;
             list-style: none;
             border-radius: 10px;
@@ -33,10 +50,17 @@ export const Container = styled.main`
             color: #fff;
             h3 {
                 margin: 6px 0 18px 0;
+                ${styledMobile500(`
+                        font-size: 0.8em;
+                    `)}
             }
             table {
                 width: 100%;
                 th, td {
+                    ${styledMobile500(`
+                        font-size: 0.7em;
+                        padding: 2px;
+                    `)}
                     border: solid 2px #fff;
                     padding: 4px;
                     text-align: center;
@@ -44,6 +68,9 @@ export const Container = styled.main`
                     border-radius: 3px;
                 }
                 th, tfoot {
+                    ${styledMobile500(`
+                        font-size: 0.9em;
+                    `)}
                     font-size: 1.3em;
                     font-weight: bold;
                 }
