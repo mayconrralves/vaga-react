419b2b7009cd885e5276462af8752a03a0da04cc
Maycon Rogers Ribeiro Alves <mayconrralves@yahoo.com.br>
2021-04-19 07:15:04 -0300

    Adição de media query para responsividade em Footer

--- web-ecommerce/src/components/Footer/styles.js
+++ web-ecommerce/src/components/Footer/styles.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 
+
 export const Container = styled.footer`
     display: flex;
     width: 100%;
@@ -11,6 +12,9 @@ export const Container = styled.footer`
     a {
         margin: 0 20px;
         svg {
+            @media screen and (max-width: 600px){
+                font-size: 2em;
+            }
             font-size: 4em;
             color: #fff;
            
