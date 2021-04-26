05230a953656117bb0de1a1c8e236f1dbdd867c9
Maycon Rogers Ribeiro Alves <mayconrralves@yahoo.com.br>
2021-04-26 12:13:05 -0300

    Adição de verficação de formulário com Yup

--- web-ecommerce/src/components/SignIn/index.js
+++ web-ecommerce/src/components/SignIn/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect} from 'react';
+import React, { useEffect, useState} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import * as Yup from 'yup';
@@ -9,17 +9,18 @@ import Title from '../Title';
 import Loading from '../Loading';
 import { toastMessageError } from '../../functions/toastError';
 
-
 export default function SignIn(){
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
 	const { loading, fail, msgError } = useSelector(state=> state.auth);
 	const dispatch = useDispatch();
-	
+	const schemaLogin = Yup.object().shape({
+		email: Yup.string().email("Digite um email válido").required("Email é requerido"),
+		password: Yup.string().required("Password é requerido"),
+	});
 	useEffect(()=> {
-		if(fail){
-			toastMessageError(msgError)
-		}
+		if(fail) toastMessageError(msgError);
 	}, [fail]);
-	console.log(fail, loading)
 	return (
 			<Container>
 				{
@@ -33,17 +34,23 @@ export default function SignIn(){
 							</div>
 							<Formik
 								initialValues={{
-									email: '',
-									password:'',
+									email,
+									password
 								}}
+								enableReinitialize={false}
+								validationSchema={schemaLogin}
 								onSubmit={ ( values ) => {
 									const {email, password } = values;
+									setEmail(email);
+									setPassword(password);
 									dispatch(signInRequest(email, password));
 								}}
 							>
 								<Form>
-									<Field name='email' type='email' placeholder='Seu email...' />				
-									<Field name='password' type='password' placeholder='Sua senha...' />				
+									<Field name='email' type='email' placeholder='Seu email...' />
+									<p><ErrorMessage name="email" /></p>				
+									<Field name='password' type='password' placeholder='Sua senha...' />
+									<p><ErrorMessage name="password" /></p>
 									<Field name='signin' type='submit' value='Enviar' />				
 								</Form>
 							</Formik>
